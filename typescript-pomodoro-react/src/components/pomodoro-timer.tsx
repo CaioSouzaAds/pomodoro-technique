87e1cf5696910c16a83dcac8a528b8b51c3d05df
CaioSouzaAds <caio.cesarads@gmail.com>
2023-12-27 15:51:07 -0300

    Manage time and working mode state, apply body class for working, and implement work configuration with buttons.

--- typescript-pomodoro-react/src/components/pomodoro-timer.tsx
+++ typescript-pomodoro-react/src/components/pomodoro-timer.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useInterval } from '../hooks/use-interval';
 import { Button } from './button';
 import { Timer } from './timer';
+import { time } from 'console';
 
 interface PomodoroTimerProps {
   pomodoTime: number;
@@ -12,24 +13,41 @@ interface PomodoroTimerProps {
 
 export function PomodoroTimer(props: PomodoroTimerProps) {
   const [mainTime, setMainTime] = React.useState(props.pomodoTime);
+  const [timeCounting, setTimeCounting] = React.useState(false);
+  const [working, setWorking] = React.useState(false);
 
-  useInterval(() => {
-    setMainTime(mainTime - 1);
-  }, 1000);
+  useEffect(() => {
+    if (working) document.body.classList.add('working');
+  }, [working]);
+
+  useInterval(
+    () => {
+      setMainTime(mainTime - 1);
+    },
+    timeCounting ? 1000 : null,
+  );
+
+  const configureWork = () => {
+    setTimeCounting(true);
+    setWorking(true);
+  };
 
   return (
     <div className="pomodoro">
       <h2>You are: working</h2>
       <Timer mainTime={mainTime}></Timer>
       <div className="controls">
+        <Button text="Work" onClick={() => configureWork()}></Button>
         <Button text="teste" onClick={() => alert('teste')}></Button>
-        <Button text="teste" onClick={() => alert('teste')}></Button>
-        <Button text="teste" onClick={() => alert('teste')}></Button>
+        <Button
+          text={timeCounting ? 'Pause' : 'Play'}
+          onClick={() => setTimeCounting(!timeCounting)}
+        ></Button>
       </div>
 
       <div className="details">
         <p>
-          is simply dummy text of the printing and typesetting industry. Lorem
+          Is simply dummy text of the printing and typesetting industry. Lorem
           Ipsum has been the industry's standard dummy text ever since the
           1500s, when an unknown printer took a galley of type and scrambled it
           to make a type specimen book.
